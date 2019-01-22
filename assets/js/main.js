d03614619f432c461200c0e871ef7a1f0d8d2655
alexmquinlan <alexquinlan1@gmail.com>
2019-01-21 20:39:35 -0500

    added Work page and updated some of the responsive behaviors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var chartCreated = false;
+
 $(document).ready(function() {
 
 	var direction = 'top',
@@ -8,11 +10,18 @@ $(document).ready(function() {
 			return;
 		clicked = true;
 		var color = $(this).css("color");
-		console.log(color);
 		var winHeight = $(window).height();
 
-		var newDir = $(this).attr('name');
+		var page = $(this).html().toLowerCase(); // get page name
+		var source   = $("#"+page+"-template").html(); // get template for page
+		var template = Handlebars.compile(source);
 
+		$("#handlebarsTemplate").html(template);
+
+		if (!chartCreated && page === "work")
+			createSkillsChart();
+
+		var newDir = $(this).attr('name');
 		
 		if (newDir != direction)
 			$('.hidden-page').addClass('hp-' + newDir)
@@ -70,5 +79,46 @@ $(document).ready(function() {
 		});
 	});
 
-
 });
+
+
+function createSkillsChart() {
+	chartCreated = true;
+	var ctx = $("#skillsChart")[0];
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["C++", "Java", "Python", "HTML/CSS", "JavaScript", "SQL"],
+	        datasets: [{
+	            label: 'Years',
+	            data: [3, 4, 2.5, 5, 5, 4],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
